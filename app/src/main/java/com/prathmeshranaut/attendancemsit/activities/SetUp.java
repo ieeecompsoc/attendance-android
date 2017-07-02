6743c047d6b7ef6cae7660ef5ddcdc94356adddf
Jatin Bindra <jatinbindra171998@gmail.com>
2017-07-03 00:53:38 +0530

    API_hit

--- app/src/main/java/com/prathmeshranaut/attendancemsit/activities/SetUp.java
+++ app/src/main/java/com/prathmeshranaut/attendancemsit/activities/SetUp.java
@@ -2,8 +2,19 @@ package com.prathmeshranaut.attendancemsit.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.prathmeshranaut.attendancemsit.R;
+import com.prathmeshranaut.attendancemsit.general.ApiClient;
+import com.prathmeshranaut.attendancemsit.general.ApiInterface;
+import com.prathmeshranaut.attendancemsit.general.ReadingResponse;
+import com.prathmeshranaut.attendancemsit.general.Student_single;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class SetUp extends AppCompatActivity {
 
@@ -11,5 +22,34 @@ public class SetUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_up);
+        FetchStudents();
+    }
+
+    private void FetchStudents() {
+        try {
+
+
+
+            ApiInterface apiInterface= ApiClient.getApiInterface();
+String header="";
+            Call<ArrayList<ReadingResponse>> showsettings = apiInterface.showsettings(header);
+            showsettings.enqueue(new Callback<ArrayList<ReadingResponse>>() {
+                @Override
+                public void onResponse(Call<ArrayList<ReadingResponse>> call, Response<ArrayList<ReadingResponse>> response) {
+
+                }
+
+                @Override
+                public void onFailure(Call<ArrayList<ReadingResponse>> call, Throwable t) {
+
+                }
+
+
+            });
+        } catch (Exception e) {
+            Log.d("mmm",e.getMessage().toString());
+
+            e.printStackTrace();
+        }
     }
 }
